8098244edab6da49e7971d9002baf8d11c22c759
Utkarsh Gaur <charmedtobe@gmail.com>
2018-01-26 21:20:42 -0500

    Basic text checker with score but no sanitization or edge case.

---
+++ Assets/KeyText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+ * Use this class for the special text. 
+ * It holds one sentence
+ * + keywords
+ * + verifier that keywords are in the sentence
+ * + verifier that keywords are unique
+ * + verifier that chars are only letters.
+ * 
+ */
+
+public class KeyText{
+	public string Sentence;
+	public string[] KeyWords;
+
+	public KeyText()
+	{
+		Sentence = "This is a special sentence. It has text.";
+		KeyWords = new string[] {"special", "text", "It"};
+	}
+}
---
+++ Assets/Texter.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+/*
+ * The class that reads in the
+ * text input, and compares it
+ * against the KeyText, and returns
+ * the score.
+ * 
+ */
+
+public class Texter : MonoBehaviour {
+
+	public KeyText keyText;
+	public Text inText;
+	public Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void CheckTextForKey(){
+		int score = 0;
+		string hits = "";
+
+		keyText = new KeyText ();
+
+		string pattern = @" ";
+		string[] words = Regex.Split (inText.text, pattern);
+
+		foreach (string word in words) {
+			foreach (string keyword in keyText.KeyWords) {
+				if (word == keyword) {
+					Debug.Log ("Matched: " + word);
+					score++;
+					hits += word + "\n";
+				}
+			}
+
+		}
+
+		scoreText.text = hits + "Score:" + score.ToString ();
+		
+	}
+
+}
